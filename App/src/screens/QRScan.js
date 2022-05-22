a741d6f62b8cba913c35045793120171300ec345
fenix1851 <foxyess@mail.ru>
2022-05-22 08:29:15 +0300

    q2

--- App/src/screens/QRScan.js
+++ App/src/screens/QRScan.js
@@ -1,103 +1,82 @@
-import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Button,
-  Image,
-} from "react-native";
-import { useEffect, useRef, useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import Constants from "expo-constants";
 import { Camera } from "expo-camera";
-import { shareAsync } from "expo-sharing";
-import * as MediaLibrary from "expo-media-library";
+
+// You can import from local files
+import AssetExample from "./components/AssetExample";
+
+// or any pure javascript modules available in npm
+import { Card } from "react-native-paper";
 
 export default function App() {
-  let cameraRef = useRef();
-  const [hasCameraPermission, setHasCameraPermission] = useState();
-  const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState();
-  const [photo, setPhoto] = useState();
+  useEffect((f) => {
+    (async (f) => {
+      try {
+        const hasCam = await Camera.isAvailableAsync().catch(
+          (e) => `Error: ${e.message}`
+        );
+        const cameras = await Camera.getAvailableCameraTypesAsync().catch(
+          (e) => `Error: ${e.message}`
+        );
+        const permissions = await Camera.getPermissionsAsync().catch(
+          (e) => `Error: ${e.message}`
+        );
 
-  useEffect(() => {
-    (async () => {
-      const cameraPermission = await Camera.requestCameraPermissionsAsync();
-      const mediaLibraryPermission = await MediaLibrary.getPermissionsAsync();
-      setHasCameraPermission(cameraPermission.status === "granted");
-      setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
+        console.log(
+          "Device has cam: ",
+          hasCam,
+          cameras,
+          " permissions :",
+          permissions
+        );
+
+        if (!permissions.granted) {
+          console.log("No permissions yet, asking");
+          const newPermissions = await Camera.requestPermissionsAsync();
+          console.log("Result: ", newPermissions);
+        }
+      } catch (e) {
+        console.log("Permissions error: ", e);
+      }
     })();
-  }, []);
-
-  if (hasCameraPermission === undefined) {
-    return <Text>Requesting permissions...</Text>;
-  } else if (!hasCameraPermission) {
-    return (
-      <Text>
-        Permission for camera not granted. Please change this in settings.
-      </Text>
-    );
-  }
-
-  let takePic = async () => {
-    let options = {
-      quality: 1,
-      base64: true,
-      exif: false,
-    };
-
-    let newPhoto = await cameraRef.current.takePictureAsync(options);
-    setPhoto(newPhoto);
-  };
-
-  if (photo) {
-    let sharePic = () => {
-      shareAsync(photo.uri).then(() => {
-        setPhoto(undefined);
-      });
-    };
-
-    let savePhoto = () => {
-      MediaLibrary.saveToLibraryAsync(photo.uri).then(() => {
-        setPhoto(undefined);
-      });
-    };
-
-    return (
-      <SafeAreaView style={styles.container}>
-        <Image
-          style={styles.preview}
-          source={{ uri: "data:image/jpg;base64," + photo.base64 }}
-        />
-        <Button title="Share" onPress={sharePic} />
-        {hasMediaLibraryPermission ? (
-          <Button title="Save" onPress={savePhoto} />
-        ) : undefined}
-        <Button title="Discard" onPress={() => setPhoto(undefined)} />
-      </SafeAreaView>
-    );
-  }
+  });
 
   return (
-    <Camera style={styles.container} ref={cameraRef}>
-      <View style={styles.buttonContainer}>
-        <Button title="Take Pic" onPress={takePic} />
-      </View>
-      <StatusBar style="auto" />
-    </Camera>
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>
+        Change code in the editor and watch it change on your phone! Save to get
+        a shareable url.
+      </Text>
+      <Card>
+        <Camera
+          ratio="16:9"
+          autofocus={Camera.Constants.AutoFocus.on}
+          style={{
+            width: 500,
+            height: 500,
+            maxWidth: "100%",
+            borderWidth: 10,
+            borderColor: "red",
+          }}
+        />
+      </Card>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
     justifyContent: "center",
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: "#ecf0f1",
+    padding: 8,
   },
-  buttonContainer: {
-    backgroundColor: "#fff",
-    alignSelf: "flex-end",
-  },
-  preview: {
-    alignSelf: "stretch",
-    flex: 1,
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
