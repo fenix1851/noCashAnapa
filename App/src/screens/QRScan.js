4d386746caff3de4abe0cb0af7a26545ccdbd9c5
fenix1851 <foxyess@mail.ru>
2022-05-22 08:31:20 +0300

    fds

--- App/src/screens/QRScan.js
+++ App/src/screens/QRScan.js
@@ -4,7 +4,6 @@ import Constants from "expo-constants";
 import { Camera } from "expo-camera";
 
 // You can import from local files
-import AssetExample from "./components/AssetExample";
 
 // or any pure javascript modules available in npm
 import { Card } from "react-native-paper";
